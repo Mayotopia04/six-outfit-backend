8fef19d1dd7ee0b372e874eed002bf81129aedd9
Mayotopia <134679840+Mayotopia04@users.noreply.github.com>
2024-07-13 20:04:45 +0800

    commit one

---
+++ schemas/index.js
@@ -0,0 +1,23 @@
+const {
+  registerSchema,
+  loginSchema,
+  googleLoginSchema,
+  refreshSchema,
+  passwordSchema,
+  resendEmailSchema,
+} = require("./user");
+
+const { mealSchema, dailyMealsSchema } = require("./dailyNutrition");
+const { dailyIntakeJoiSchema } = require("./dailyIntakeJoiSchema");
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+  googleLoginSchema,
+  refreshSchema,
+  passwordSchema,
+  resendEmailSchema,
+  mealSchema,
+  dailyMealsSchema,
+  dailyIntakeJoiSchema,
+};
